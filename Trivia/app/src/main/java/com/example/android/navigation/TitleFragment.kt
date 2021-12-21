563d68423e13078765bf68f5e23b65517dc791b4
Subbu <svenkat683@gmail.com>
2021-12-21 23:20:12 +0530

    Use navigation graph to navigate from one fragment to another

--- Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -19,6 +19,9 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener{ view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment2)
+        }
         return binding.root
     }
 }
